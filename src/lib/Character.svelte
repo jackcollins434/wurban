bd4f296774245367d61d009edfacd62280eca0fd
Jack Collins <jackcollins434@gmail.com>
2023-08-23 11:01:22 -0400

    Change font color for character placeholder

--- src/lib/Character.svelte
+++ src/lib/Character.svelte
@@ -3,14 +3,14 @@
   import type { HTMLAttributes, HTMLButtonAttributes } from "svelte/elements";
 
   const character = cva(
-    "shadow-lg flex justify-center border text-white px-2 py-4 w-8 md:w-9 text-lg rounded-sm font-bold relative cursor-pointer",
+    "shadow-lg flex justify-center border px-2 py-4 w-8 md:w-9 text-lg rounded-sm font-bold relative cursor-pointer",
     {
       variants: {
         intent: {
           unused: "border-gray-300 bg-gray-300 text-black",
-          correct: "border-green-600 bg-green-600",
-          incorrect: "border-gray-800 bg-gray-800",
-          wrongPosition: "border-yellow-500 bg-yellow-500",
+          correct: "border-green-600 bg-green-600 text-white",
+          incorrect: "border-gray-800 bg-gray-800 text-white",
+          wrongPosition: "border-yellow-500 bg-yellow-500 text-white",
         },
       },
     }
